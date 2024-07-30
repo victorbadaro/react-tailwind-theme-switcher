457e6b734bd7ebeff44c185f185375ae9c9c3de9
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-07-30 16:34:30 -0300

    feat: add a very first working theme switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import { useState } from 'react';
+
 export function App() {
-	return <h1>Hello, Tailwind!</h1>;
+	const [isDarkModeEnabled, setIsDarkModeEnabled] = useState(true);
+
+	function toggleTheme() {
+		setIsDarkModeEnabled(prevState => !prevState);
+	}
+
+	return (
+		<div className={`${isDarkModeEnabled ? 'dark ' : ''}min-h-screen bg-zinc-100 dark:bg-zinc-900 text-zinc-900 dark:text-zinc-100`}>
+			<h1>Hello, Tailwind!</h1>
+			<button type="button" onClick={toggleTheme}>change theme</button>
+		</div>
+	);
 }
